ec0c543e259fa73aecab633482854e2224a0d1f3
Daisuke Maki <dmaki@cpan.org>
2005-06-14 07:26:23 +0000

    Import of DMAKI/XML-RSS-LibXML-0.01 from CPAN.
    gitpan-cpan-distribution: XML-RSS-LibXML
    gitpan-cpan-version:      0.01
    gitpan-cpan-path:         DMAKI/XML-RSS-LibXML-0.01.tar.gz
    gitpan-cpan-author:       DMAKI
    gitpan-cpan-maturity:     released

--- lib/XML/RSS/LibXML.pm
+++ lib/XML/RSS/LibXML.pm
@@ -1,4 +1,4 @@
-# $Id: LibXML.pm 3 2005-06-14 04:52:03Z daisuke $
+# $Id: LibXML.pm 4 2005-06-14 07:13:20Z daisuke $
 #
 # Daisuke Maki <dmaki@cpan.org>
 # All rights reserved.
@@ -21,11 +21,17 @@ my %namespaces = (
     rss09   => "http://my.netscape.com/rdf/simple/0.9/",
 );
 
-my %xpath_catalog = (
+my %ParseContext = (
     channel => {
         link        => [ qw(link @rdf:about) ],
         creator     => [ qw(dc:creator) ],
+        contributor => [ qw(dc:contributor) ],
+        coverage    => [ qw(dc:coverage) ],
         title       => [ qw(title dc:title) ],
+        language    => [ qw(language) ],
+        copyright   => [ qw(dc:rights copyright) ],
+        generator   => [ qw(admin:generatorAgent generator) ],
+        identifier  => [ qw(dc:identifier) ],
     },
     item => {
         # run this only on <item> elements
@@ -39,7 +45,7 @@ my %xpath_catalog = (
     }
 );
 
-my %version_prefix = (
+my %VersionPrefix = (
     '1.0' => 'rss10',
     '0.9' => 'rss09',
 );
@@ -51,10 +57,58 @@ sub new
 
     my $p = XML::LibXML->new();
     $p->recover(1);
-    bless {
+
+    my $c = XML::LibXML::XPathContext->new();
+    my $self = bless {
         _parser => $p,
+        _context => $c,
+        _pctxt   => { channel => {}, item => {} },
     }, $class;
+
+    $self->_init();
+    return $self;
 }
+
+sub _init
+{
+    my $self = shift;
+
+    # Register namespaces.
+    while (my($prefix, $uri) = each %namespaces) {
+        $self->add_module(prefix => $prefix, uri => $uri);
+    }
+
+    # Register parse contexts.
+    while (my($context, $map) = each %ParseContext) {
+        while (my($field, $xpath_list) = each %$map) {
+            foreach my $xpath (@$xpath_list) {
+                $self->add_parse_context(context => $context, field => $field, xpath => $xpath);
+            }
+        }
+    }
+}
+
+sub add_module
+{
+    my $self = shift;
+    my %args = @_;
+    $self->{_context}->registerNs($args{prefix}, $args{uri});
+}
+
+sub add_parse_context
+{
+    my $self = shift;
+    my %args = @_;
+
+    my $context = lc($args{context});
+    my $field   = lc($args{field});
+    my $xpath   = $args{xpath};
+
+    my $pctxt = $self->{_pctxt};
+    $pctxt->{$context}->{$field} ||= [];
+    push @{$pctxt->{$context}->{$field}}, $xpath;
+}
+
 
 sub parse
 {
@@ -63,7 +117,7 @@ sub parse
     my $p = $self->{_parser};
     my $dom = $p->parse_string(@_);
     $self->{_dom} = $dom;
-    $self->_analyze();
+    $self->_parse_dom;
 
     return $self;
 }
@@ -75,15 +129,26 @@ sub parsefile
     my $p = $self->{_parser};
     my $dom = $p->parse_file(@_);
     $self->{_dom} = $dom;
+    $self->_parse_dom;
 }
 
-sub _analyze
+sub as_string
+{
+    my $self = shift;
+    return $self->{_dom} ? $self->{_dom}->toString(1) : undef;
+}
+
+sub _elem { $_[0]->{$_[1]} }
+sub channel { shift->_elem('channel') }
+sub items   { shift->_elem('items')   }
+
+sub _parse_dom
 {
     my $self = shift;
 
     $self->{_internal}->{version} = $self->_guess_version();
-    $self->{channel} = $self->channel;
-    $self->{items} = $self->items;
+    $self->{channel} = $self->_parse_channel;
+    $self->{items} = $self->_parse_items;
 }
 
 sub _xpath_context
@@ -126,8 +191,8 @@ sub _grab_value
     my $xpath;
     my $version = $self->{_internal}->{version};
     foreach my $xp (@$candidates) {
-        if ($xp !~ /^[^:]+:/ && $version_prefix{$version}) {
-            $xpath = $version_prefix{$version} . ":$xp";
+        if ($xp !~ /^[^:]+:/ && $VersionPrefix{$version}) {
+            $xpath = $VersionPrefix{$version} . ":$xp";
         } else {
             $xpath = $xp;
         }
@@ -142,7 +207,7 @@ my %ChannelRoot = (
     '0.9'   => '/rdf:RDF/rss09:channel',
     'other' => '/rss/channel'
 );
-sub channel
+sub _parse_channel
 {
     my $self = shift;
     my %args = @_;
@@ -153,9 +218,14 @@ sub channel
     my $root_xpath = $ChannelRoot{$version} || $ChannelRoot{other};
     my $xc = $self->_xpath_context();
     my($channel) = $xc->findnodes($root_xpath, $dom);
-    
-    foreach my $want qw(title link description) {
-        $channel{$want} = $self->_grab_value($channel, $xc, $xpath_catalog{channel}{$want});
+
+    my $value;
+
+    my $want = $self->{_pctxt}->{channel};
+    while (my($field, $xpath_list) = each %$want) {
+        if (defined($value = $self->_grab_value($channel, $xc, $xpath_list))) {
+            $channel{$field} = $value;
+        }
     }
 
     return \%channel;
@@ -166,7 +236,7 @@ my %ItemRoot = (
     '0.9'   => '/rdf:RDF/rss09:item',
     'other' => '/rss/channel/item'
 );
-sub items
+sub _parse_items
 {
     my $self = shift;
 
@@ -176,12 +246,15 @@ sub items
     my $version = $self->{_internal}->{version};
     my $root_xpath = $ItemRoot{$version} || $ItemRoot{other};
     my $xc = $self->_xpath_context;
+
+    my $want = $self->{_pctxt}->{item};
     foreach my $item ($xc->findnodes($root_xpath, $dom)) {
         my %item;
         my $value;
-        foreach my $want qw(link title description content issued modified creator) {
-            if (defined($value = $self->_grab_value($item, $xc, $xpath_catalog{item}{$want}))) {
-                $item{$want} = $value;
+
+        while (my($field, $xpath_list) = each %$want) {
+            if (defined($value = $self->_grab_value($item, $xc, $xpath_list))) {
+                $item{$field} = $value;
             }
         }
         push @items, \%item;
@@ -209,21 +282,37 @@ XML::RSS::LibXML - XML::RSS with XML::LibXML (parse-only)
      print "  item: $item->{title} ($item->{link})\n";
   }
 
+  # Add custom modules
+  $rss->add_module(uri => $uri, prefix => $prefix);
+
+  # Add custom parse contexts
+  $rss->add_parse_context(
+    context => $context, # 'channel', 'item'
+    field   => $field_name,
+    xpath   => $xpath
+  );
+  $rss->parse(...); # now parse with new context
+
 =head1 DESCRIPTION
 
+XML::RSS::LibXML uses XML::LibXML (libxml2) for parsing RSS instead of XML::RSS'
+XML::Parser (expat), while trying to keep interface compatibility with XML::RSS.
+
 XML::RSS is an extremely handy tool, but it is unfortunately not exactly the
-most lean or efficient RSS parser, especially in a long-running process. So
-for a long time I had been using my own version of RSS parser to get the maximum
-speed and efficiency - this is the re-packaged version of such module, such that
-it adheres to the XML::RSS interface.
+most lean or efficient RSS parser, especially in a long-running process.
+So for a long time I had been using my own version of RSS parser to get the
+maximum speed and efficiency - this is the re-packaged version of that module,
+such that it adheres to the XML::RSS interface.
 
-It uses XML::LibXML as the underlying XML parser, and is therefore much much
-faster than XML::RSS
+XML::RSS::LibXML is B<NOT> 100% compatible with XML::RSS. 
+For example, XML::RSS::LibXML is not capable of outputting RSS in
+various formats, and namespaces aren't exactly supported the way they are
+in XML::RSS (patches welcome).
 
-YMMV, but in reality, I do not parse RSS files this way -- because where 
-performance matters, it is important to *NOT* store unnecessary data in memory.
-So do note that while this module may achieve what XML::RSS does faster, but
-it's not the solution. 
+Use this module when you have severe performance requirements in parsing
+RSS files.
+
+=head1 PARSED FIELDS
 
 =head1 METHODS
 
@@ -238,6 +327,52 @@ Parse a string containing RSS.
 =head2 parse_file($filename)
 
 Parse an RSS file specified by $filename
+
+=head2 as_string()
+
+Return the string representation of the parsed RSS.
+
+=head2 add_module(uri =E<lt> $uri, prefix =E<lt> $prefix)
+
+Adds a new module. You should do this before parsing the RSS.
+XML::RSS::LibXML understands a few modules by default:
+
+    rdf     => "http://www.w3.org/1999/02/22-rdf-syntax-ns#",
+    dc      => "http://purl.org/dc/elements/1.1/",
+    sy      => "http://purl.org/rss/1.0/modules/syndication/",
+    admin   => "http://webns.net/mvcb/",
+    content => "http://purl.org/rss/1.0/modules/content/",
+    cc      => "http://web.resource.org/cc/",
+    taxo    => "http://purl.org/rss/1.0/modules/taxonomy/",
+
+So you do not need to add these explicitly.
+
+=head2 add_parse_context(context =E<lt> $context, field =E<lt> $field, xpath =E<lt> $xpath)
+
+Adds new parse contexts. XML::RSS::LibXML attempts to parse most of the
+oft-used fields from RSS feeds, but often there are times when you want
+finer grain of control.
+
+If, for example, you want to include a custom field in within the 
+E<lt>channelE<gt> element called C<foo>, you may add something like this:
+
+  $rss->add_parse_context(
+    context => 'channel',
+    field   => 'foo',
+    xpath   => 'foo', # XPath relative to the current context, which is
+                      # 'channel'
+  );
+  $rss->parsefile($file);
+
+Then after parsing, $rss will contain a structure like this:
+
+  $rss = {
+    channel => {
+      foo => $value_of_foo
+      # other fields
+    },
+    # other fields
+  };
 
 =head1 PERFORMANCE
 
@@ -254,15 +389,17 @@ No support whatsover for writing RSS. No plans to support it either.
 
 =head1 TODO
 
-Tests. Currently tests are simply stolen from XML::RSS. 
+Tests. Currently tests are simply stolen from XML::RSS. It would be nice
+to have tests that do more extensive testing for correctness
 
 =head1 SEE ALSO
 
 L<XML::RSS|XML::RSS>, L<XML::LibXML|XML::LibXML>, L<XML::LibXML::XPathContext>
 
-=head1 AUTHOR
+=head1 AUTHORS
 
-Copyright 2005 Daisuke Maki E<lt>dmaki@cpan.orgE<gt>. All rights reserved.
-Development funded by Brazil, Ltd. E<lt>http://b.razil.jpE<gt>
+Copyright 2005 Daisuke Maki E<lt>dmaki@cpan.orgE<gt>, Tatsuhiko Miyagawa E<lt>miyagawa@bulknews.netE<gt>. All rights reserved.
+
+Development partially funded by Brazil, Ltd. E<lt>http://b.razil.jpE<gt>
 
 =cut
